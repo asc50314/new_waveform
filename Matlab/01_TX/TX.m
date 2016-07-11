a194681e712c0b4c96590978893d96c7b4ef385c
Friedrich <asc50314200@gmail.com>
2016-07-11 09:33:25 +0800

    Tone selection

--- Matlab/01_TX/TX.m
+++ Matlab/01_TX/TX.m
@@ -8,21 +8,29 @@ clear all; close all; clc;
 %--------------------------------------------------------------------------
 % Mode Parameters
 %--------------------------------------------------------------------------
-Mode.Trans = 'OFDM'; % OFDM/WOLA/FBMC/UFMC
+Mode.Trans = 'WOLA'; % OFDM/WOLA/FBMC/UFMC
 %--------------------------------------------------------------------------
 % Execution Parameters
 %--------------------------------------------------------------------------
-Param.run = 1;
-Param.sample_rate = 1200;
-Param.SymbolNum = 60;
-Param.FFTSize = 12;
-Param.CPLength = 1;
+Param.run             = 1;
+Param.sample_rate     = 1200;
+Param.SymbolNum       = 1;
+Param.FFTSize         = 1024;
+Param.CPLength        = round(Param.FFTSize*0.1); %CP number of sample after symbol up-sample
+Param.BandStart       = 256;
+Param.ToneNum         = 12;
 
-Parm.SymbolUpSample = 4;
-Parm.PulseShape  =rcosine(1, Param.SymbolUpSample, 'fir', 0.25, 4);
+Param.SymbolUpSample  = 1;
+Param.SymbolInterpoDelay  = 4;
+if(Param.SymbolUpSample > 1)
+  Param.SymbolInterpoFunc   = rcosine(1, Param.SymbolUpSample, 'fir', 0.25, Param.SymbolInterpoDelay);
+end
 
-Param.UpSampleDAC = 8;
-Parm.DAC_LPF  =rcosine(1, Param.UpSampleDAC, 'fir', 0.078, 3);
+Param.UpSampleDAC     = 8;
+Param.DAC_LPF         = rcosine(1, Param.UpSampleDAC, 'fir', 0.078, 3);
+
+%For WOLA
+Param.RollOffPeriod   = round(Param.FFTSize*0.0781/2)*2; %Number of sample after symbol up-sample
 
 %--------------------------------------------------------------------------
 % Frame Generating
@@ -32,30 +40,30 @@ for run_count = 1:Param.run
     Frame(run_count) = frame_gen(Mode,Param);
 end
 
-%--------------------------------------------------------------------------
-% Frame PSD
-%--------------------------------------------------------------------------
-Frame_TX = zeros(1,780);
-for run_count = 1:Param.run
-    Frame_TX = Frame_TX+Frame(run_count).Frame_TX;
-end
-Frame_TX = Frame_TX/Param.run;
+% %--------------------------------------------------------------------------
+% % Frame PSD
+% %--------------------------------------------------------------------------
+% Frame_TX = zeros(1,780);
+% for run_count = 1:Param.run
+%     Frame_TX = Frame_TX+Frame(run_count).Frame_TX;
+% end
+% Frame_TX = Frame_TX/Param.run;
 
-% fd_Frame = fftshift(fft(Frame_TX));
-% plot(abs(fd_Frame))
-a =  zeros(1,Param.UpSampleDAC*length(Frame_TX));
-a(1:Param.UpSampleDAC:end) = Frame_TX;
+% % fd_Frame = fftshift(fft(Frame_TX));
+% % plot(abs(fd_Frame))
+% a =  zeros(1,Param.UpSampleDAC*length(Frame_TX));
+% a(1:Param.UpSampleDAC:end) = Frame_TX;
 
-a = conv(h, a);
+% a = conv(h, a);
 
-psd_Frame = fftshift(fft(a));
-% figure(2)
-% plot(abs(psd_Frame))
-psd_Frame = abs(psd_Frame).^2;
-psd_Frame = psd_Frame./max(psd_Frame);
-f_spacing = Param.sample_rate/Param.FFTSize;
-freq = -f_spacing/2:f_spacing/(length(psd_Frame)-1):f_spacing/2;
-% figure(3)
+% psd_Frame = fftshift(fft(a));
+% % figure(2)
+% % plot(abs(psd_Frame))
+% psd_Frame = abs(psd_Frame).^2;
+% psd_Frame = psd_Frame./max(psd_Frame);
+% f_spacing = Param.sample_rate/Param.FFTSize;
+% freq = -f_spacing/2:f_spacing/(length(psd_Frame)-1):f_spacing/2;
+% % figure(3)
 
-plot(freq,10*log10(psd_Frame));    
-grid on
+% plot(freq,10*log10(psd_Frame));    
+% grid on
--- Matlab/01_TX/frame_gen.m
+++ Matlab/01_TX/frame_gen.m
@@ -3,53 +3,72 @@ function [Frame]=frame_gen(Mode,Param)
 %-----------------------------------------------------
 % Code for PHY Payload Field
 %-----------------------------------------------------
-%-- Aid structure parameter setting
+%-- Symbol aid structure parameter setting
+switch Mode.Trans
+  case 'WOLA'
+    %-- Weighting function
+    ProtoFl = ones(1,Param.FFTSize+Param.CPLength);
+    WeightFunc = sin(pi*(0:Param.RollOffPeriod-1)/Param.RollOffPeriod)/sum(sin(pi*(0:Param.RollOffPeriod-1)/Param.RollOffPeriod));
+    WeightFunc = conv(ProtoFl,WeightFunc);
+    WeightFunc = WeightFunc(1:Param.RollOffPeriod);
+    clear ProtoFl;
+end
 
-
-
-%-- Original Data Bit Stream (16QAM)
-<<<<<<< HEAD
 for run_count = 1:Param.run
-  Frame.Data_Bitstream = randi([0,1],[4,Param.SymbolNum*Param.FFTSize]);
-
+  %Frame initializing
+  switch Mode.Trans
+    case 'OFDM'
+      Frame.Frame_TX = [];
+    case 'WOLA'
+      Frame.Frame_TX = zeros(1,Param.RollOffPeriod/2);
+  end
+  %-- Original Data Bit Stream (16QAM)
+  Frame.Data_Bitstream = randi([0,1],[4,Param.SymbolNum*Param.ToneNum]);
   Symbol16QAM = zeros(1,Param.FFTSize);
-
   for symbol_count = 1:Param.SymbolNum
     %-- Constellation Mapping (16QAM)
-    gray_16QAM_map = [-3-3i -1-3i 1-3i 3-3i ...
-                      -3-i  -1-i  1-i  3-i ...
+    Gray16QAMmap = [  -3-3i -1-3i 1-3i 3-3i ...
+                      -3-i  -1-i  1-i  3-i  ...
                       -3+3i -1+3i 1+3i 3+3i ...
-                      -3+i  -1+i  1+i  3+i];
-    for data_count = 1:Param.FFTSize  % random sequence mapping to 16QAM
-      index = num2str([Frame.Data_Bitstream(:,data_count+(symbol_count-1)*Param.FFTSize)]);
-      index = bin2dec(index.');
-      Symbol16QAM(data_count) = gray_16QAM_map(index+1)/10^0.5;
+                      -3+i  -1+i  1+i  3+i  ];
+    Symbol16QAM = zeros(1,Param.FFTSize);
+    for data_count = Param.BandStart:Param.BandStart+Param.ToneNum-1  % random sequence mapping to 16QAM
+      GrayIndex = num2str([Frame.Data_Bitstream(:,data_count+(symbol_count-1)*Param.FFTSize)]);
+      GrayIndex = bin2dec(GrayIndex .');
+      Symbol16QAM(data_count) = Gray16QAMmap(GrayIndex+2)/10^0.5;
     end
-
-    %-----------------------------------------------------
-    % Symbol Generating
-    %-----------------------------------------------------
+    %-- Time-domain Symbol Generating
     %-- iFFT
     SymbolTD = ifft(Symbol16QAM);
-    %-- Symbol up-sample
-    for data_count = 1:Param.FFTSize
-      SymbolTDup((data_count-1)*Param.SymbolUpSample+1:(data_count)*Param.SymbolUpSample) = [SymbolTD(data_count) 0 0 0];
+
+    if(Param.SymbolUpSample > 1)
+      %-- Symbol Up-sample
+      for data_count = 1:Param.FFTSize
+        SymbolTDup((data_count-1)*Param.SymbolUpSample+1:(data_count)*Param.SymbolUpSample) = [SymbolTD(data_count) zeros(1,Param.SymbolUpSample-1)];
+      end
+      %-- RC Interpolation
+      SymbolTDupInterpo = cconv(SymbolTDup, Param.SymbolInterpoFunc, length(SymbolTDup));
+      SymbolTDupInterpo = circshift(SymbolTDupInterpo,[0 -Param.SymbolUpSample*Param.SymbolInterpoDelay]);
+      SymbolTD = SymbolTDupInterpo;
+      clear SymbolTDupInterpo;
     end
-    %-- RC interpolation
-    SymbolTDup = conv(SymbolTDup, Parm.PlusShap);
-    SymbolTDup = SymbolTDup(17:end-16);
-    %-- Adding aid structure for specific transmission 
+    %-- Adding aid structure for specific transmission
     switch Mode.Trans
       case 'OFDM'
         %-- Add CP
         SymbolTD = [SymbolTD(end-Param.CPLength+1:end) SymbolTD]; 
-        Frame.Frame_TX((symbol_count-1)*(Param.CPLength+Param.FFTSize)+1:symbol_count*(Param.CPLength+Param.FFTSize)) = SymbolTD;
+        % Frame.Frame_TX((symbol_count-1)*(Param.CPLength+Param.FFTSize)+1:symbol_count*(Param.CPLength+Param.FFTSize)) = SymbolTD;
+        Frame.Frame_TX(end+1:end+length(SymbolTD)) = SymbolTD;
       case 'WOLA'
         %-- Add CP
-        SymbolTD = [SymbolTD(end-Param.CPLength+1:end) SymbolTD]; 
-        Frame.Frame_TX((symbol_count-1)*(Param.CPLength+Param.FFTSize)+1:symbol_count*(Param.CPLength+Param.FFTSize)) = SymbolTD;
-      otherwise
-
-    end     
+        SymbolTD = [SymbolTD(end-Param.CPLength-Param.RollOffPeriod/2+1:end) SymbolTD SymbolTD(1:Param.RollOffPeriod/2)];
+        SymbolTD(1:Param.RollOffPeriod) = SymbolTD(1:Param.RollOffPeriod) .* WeightFunc;
+        SymbolTD(end-Param.RollOffPeriod+1:end) = SymbolTD(end-Param.RollOffPeriod+1:end) .* fliplr(WeightFunc);
+        % Frame.Frame_TX((symbol_count-1)*(Param.CPLength+Param.FFTSize)+1:symbol_count*(Param.CPLength+Param.FFTSize)) = SymbolTD;
+        %Overlap adding
+        Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) = Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) + SymbolTD(1:Param.RollOffPeriod/2);
+        %Attach to frame
+        Frame.Frame_TX(end-Param.CPLength-Param.FFTSize-Param.RollOffPeriod/2+1:end) = SymbolTD(Param.RollOffPeriod/2+1:end);
+    end
   end
 end
