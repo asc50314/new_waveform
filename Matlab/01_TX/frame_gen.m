ff785f5b99243a72818563d14f39cd4e33b37fcb
Friedrich <asc50314200@gmail.com>
2016-07-12 09:53:09 +0800

    Frame_TX contain 1 symbol per row

--- Matlab/01_TX/frame_gen.m
+++ Matlab/01_TX/frame_gen.m
@@ -13,9 +13,6 @@ switch Mode.Trans
     WeightFunc = sin(pi*(0:Param.RollOffPeriod-1)/Param.RollOffPeriod)/sum(sin(pi*(0:Param.RollOffPeriod-1)/Param.RollOffPeriod));
     WeightFunc = conv(ProtoFl,WeightFunc);
     WeightFunc = WeightFunc(1:Param.RollOffPeriod);
-    %===============test========================
-    % WeightFunc = ones(1,length(WeightFunc));
-    %===============test========================
     clear ProtoFl;
 end
 
@@ -26,81 +23,84 @@ Gray16QAMmap = [  -3-3i -1-3i 1-3i 3-3i ...
                   -3-i  -1-i  1-i  3-i  ...
                   -3+3i -1+3i 1+3i 3+3i ...
                   -3+i  -1+i  1+i  3+i  ];
-
-% Gray16QAMmap = [  3+3i 3+3i 3+3i 3+3i...
-%                   3+3i 3+3i 3+3i 3+3i...
-%                   3+3i 3+3i 3+3i 3+3i...
-%                   3+3i 3+3i 3+3i 3+3i];
-
-
-  %Frame initializing
+%Frame initializing
+switch Mode.Trans
+  case 'OFDM'
+    Frame.Frame_TX = [];
+  case 'WOLA'
+    Frame.Frame_TX = zeros(1,Param.RollOffPeriod/2);
+end
+%Original Data Bit Stream (16QAM)
+Frame.Data_Bitstream = randi([0,1],[4,Param.SymbolNum*Param.ToneNum]);
+%-----------------------------
+% Symbol Generating
+%-----------------------------
+for symbol_count = 1:Param.SymbolNum
+  %-----------------------------
+  % Constellation Mapping (16QAM)
+  %-----------------------------
+  Symbol16QAM = zeros(1,Param.FFTSize);
+  for data_count = Param.BandStart:Param.BandStart+Param.ToneNum-1  % random sequence mapping to 16QAM
+    GrayIndex = num2str([Frame.Data_Bitstream(:,data_count-Param.BandStart+1)]);
+    GrayIndex = bin2dec(GrayIndex .');
+    Symbol16QAM(data_count) = Gray16QAMmap(GrayIndex+1)/10^0.5;
+  end
+  %===============test========================
+  % Symbol16QAM = zeros(1,Param.FFTSize);
+  % Symbol16QAM(2) = 1;
+  %===============test========================
+  %-----------------------------
+  % Time-domain Symbol Generating
+  %-----------------------------
+  %iFFT
+  SymbolTD = ifft(Symbol16QAM);
+  %Symbol Up-sample
+  if(Param.SymbolUpSample > 1)
+    for data_count = 1:Param.FFTSize
+      SymbolTDup((data_count-1)*Param.SymbolUpSample+1:(data_count)*Param.SymbolUpSample) = [SymbolTD(data_count) zeros(1,Param.SymbolUpSample-1)];
+    end
+    %RC Interpolation
+    SymbolTDupInterpo = cconv(SymbolTDup, Param.SymbolInterpoFunc, length(SymbolTDup));
+    SymbolTDupInterpo = circshift(SymbolTDupInterpo,[0 -Param.SymbolUpSample*Param.SymbolInterpoDelay]);
+    SymbolTD = SymbolTDupInterpo;
+    clear SymbolTDupInterpo;
+  end
+  %-----------------------------
+  % Adding aid structure for specific transmission
+  %-----------------------------
   switch Mode.Trans
     case 'OFDM'
-      Frame.Frame_TX = [];
-    case 'WOLA'
-      Frame.Frame_TX = zeros(1,Param.RollOffPeriod/2);
+      %-- Add CP
+      SymbolTD = [SymbolTD(end-Param.CPLength+1:end) SymbolTD];
+      %-----------------------------
+      % Real frame
+      %-----------------------------
+      % Frame.Frame_TX(end+1:end+length(SymbolTD)) = SymbolTD;
+      %-----------------------------
+      % Symbol frame
+      %-----------------------------
+      Frame.Frame_TX(end+1,:) = SymbolTD;
+    case 'WOLA' %(Refer "OFDM Versus FBMC" p.95 FILTERING)
+      %Add CPrefix and CPostfix
+      SymbolTD = [SymbolTD(end-Param.CPLength-Param.RollOffPeriod/2+1:end) SymbolTD SymbolTD(1:Param.RollOffPeriod/2)];
+      %CP weighting
+      SymbolTD(1:Param.RollOffPeriod) = SymbolTD(1:Param.RollOffPeriod) .* WeightFunc;
+      SymbolTD(end-Param.RollOffPeriod+1:end) = SymbolTD(end-Param.RollOffPeriod+1:end) .* fliplr(WeightFunc);
+      %-----------------------------
+      % Real frame
+      %-----------------------------
+      % %Symbol overlap adding
+      % Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) = Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) + SymbolTD(1:Param.RollOffPeriod/2);
+      % %Attach rest of the symbol to frame
+      % Frame.Frame_TX(end+1:end+Param.CPLength+Param.FFTSize+Param.RollOffPeriod/2) = SymbolTD(Param.RollOffPeriod/2+1:end);
+      %-----------------------------
+      % Symbol frame
+      %-----------------------------
+      %Symbol overlap adding
+      Frame.Frame_TX(end,1:Param.RollOffPeriod/2) = Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) + SymbolTD(1:Param.RollOffPeriod/2);
+      %Attach rest of the symbol to frame
+      Frame.Frame_TX(end,Param.RollOffPeriod/2+1:Param.RollOffPeriod/2+Param.CPLength+Param.FFTSize+Param.RollOffPeriod/2) = SymbolTD(Param.RollOffPeriod/2+1:end);
+      Frame.Frame_TX(end+1,:) = 0;
   end
-  %Original Data Bit Stream (16QAM)
-  Frame.Data_Bitstream = randi([0,1],[4,Param.SymbolNum*Param.ToneNum]);
-  %-----------------------------
-  % Symbol Generating
-  %-----------------------------
-  for symbol_count = 1:Param.SymbolNum
-    %-----------------------------
-    % Constellation Mapping (16QAM)
-    %-----------------------------
-    Symbol16QAM = zeros(1,Param.FFTSize);
-    for data_count = Param.BandStart:Param.BandStart+Param.ToneNum-1  % random sequence mapping to 16QAM
-      GrayIndex = num2str([Frame.Data_Bitstream(:,data_count-Param.BandStart+1)]);
-      GrayIndex = bin2dec(GrayIndex .');
-      Symbol16QAM(data_count) = Gray16QAMmap(GrayIndex+1)/10^0.5;
-    end
-    %===============test========================
-    % Symbol16QAM = zeros(1,Param.FFTSize);
-    % Symbol16QAM(2) = 1;
-    %===============test========================
-    %-----------------------------
-    % Time-domain Symbol Generating
-    %-----------------------------
-    %iFFT
-    SymbolTD = ifft(Symbol16QAM);
-    %Symbol Up-sample
-    if(Param.SymbolUpSample > 1)
-      for data_count = 1:Param.FFTSize
-        SymbolTDup((data_count-1)*Param.SymbolUpSample+1:(data_count)*Param.SymbolUpSample) = [SymbolTD(data_count) zeros(1,Param.SymbolUpSample-1)];
-      end
-      %RC Interpolation
-      SymbolTDupInterpo = cconv(SymbolTDup, Param.SymbolInterpoFunc, length(SymbolTDup));
-      SymbolTDupInterpo = circshift(SymbolTDupInterpo,[0 -Param.SymbolUpSample*Param.SymbolInterpoDelay]);
-      SymbolTD = SymbolTDupInterpo;
-      clear SymbolTDupInterpo;
-    end
-    %-----------------------------
-    % Adding aid structure for specific transmission
-    %-----------------------------
-    switch Mode.Trans
-      case 'OFDM'
-        %-- Add CP
-        SymbolTD = [SymbolTD(end-Param.CPLength+1:end) SymbolTD];
-        %--DAC Upsample and pass through LPF
-        if(Param.UpSampleDAC > 1)
-          TempSymbolTD = zeros(1,Param.UpSampleDAC*length(SymbolTD));
-          TempSymbolTD(1:Param.UpSampleDAC:end) = SymbolTD;
-          TempSymbolTD = conv(TempSymbolTD,Param.DACInterpoFunc);
-          Frame.Frame_TX(end+1:end+length(TempSymbolTD)) = TempSymbolTD;
-        else
-          Frame.Frame_TX(end+1:end+length(SymbolTD)) = SymbolTD;
-        end
-      case 'WOLA' %(Refer "OFDM Versus FBMC" p.95 FILTERING)
-        %Add CPrefix and CPostfix
-        SymbolTD = [SymbolTD(end-Param.CPLength-Param.RollOffPeriod/2+1:end) SymbolTD SymbolTD(1:Param.RollOffPeriod/2)];
-        %CP weighting
-        SymbolTD(1:Param.RollOffPeriod) = SymbolTD(1:Param.RollOffPeriod) .* WeightFunc;
-        SymbolTD(end-Param.RollOffPeriod+1:end) = SymbolTD(end-Param.RollOffPeriod+1:end) .* fliplr(WeightFunc);
-        %Symbol overlap adding
-        Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) = Frame.Frame_TX(end-Param.RollOffPeriod/2+1:end) + SymbolTD(1:Param.RollOffPeriod/2);
-        %Attach rest of the symbol to frame
-        Frame.Frame_TX(end+1:end+Param.CPLength+Param.FFTSize+Param.RollOffPeriod/2) = SymbolTD(Param.RollOffPeriod/2+1:end);
-    end
-  end
-
+end
+Frame.Frame_TX(end,:) = [];
